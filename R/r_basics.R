7e1c7aecc0d60f84e417a725dd884f0e183184ee
mcqwil <mcqwil@gmail.com>
2024-09-10 15:18:26 -0700

    comparisons and operators

--- R/r_basics.R
+++ R/r_basics.R
@@ -7,10 +7,47 @@
 42
 
 # Operators ----
+#arithmetic
+2+3  # note the spacing for legibility2
+2-3
+2*6
+2/6
+2**3  #this is an exponent
+2^3  #this is also an exponent
+
+#comparison
+2 == 2  #test for equality
+"chris" == "Chris"
+2==(1+1)
+2!=1 #test inequality, asserting that 2 is not equal 1 and r says true
+2<3
+3>2
+2<=2
+2>=3
+
+#somewhat of a cruel joke
+TRUE == 1 
+FALSE == 0
+isTRUE(TRUE) #function testing if the argument is literally true
+isTRUE(1) # 
+?isTRUE() #queries built-in help
+
+2<3 & 2>3 #will return false, both have to be true to return true
+2<3 | 1>2  # or statement
+2<3 & (2==1 | 2==2) # grouping statements for ordering
+
+#type matters (sometimes)
+"joe" # string or character
+typeof("joe")
+42  # numeric type (double or precise in decimal)
+typeof(42)
+typeof(TRUE) #logical or boolean
+42=="42" #equality can cross types
+identical(42,"42") #type matters
 
 
 
-# variables ----
+# varTRUEiables ----
 
 
 # data structures ----
